676cbcb8a36f64e8c773b256c8efaf80b868a84b
Varsha <varshadevgankar@gmail.com>
2023-12-03 22:25:38 +0530

    Main file updated

--- src/main/java/com/mindlease/fa/Application.java
+++ src/main/java/com/mindlease/fa/Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+
+		System.out.println("----------Application started-------------------");
 		SpringApplication.run(Application.class, args);
 	}
 
